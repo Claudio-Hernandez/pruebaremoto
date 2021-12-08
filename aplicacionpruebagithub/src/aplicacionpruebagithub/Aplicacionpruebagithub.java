43851ca6479a8e30c6804544c363dd9f2e188564
Claudio-Hernandez <claudio.ahz123@gmail.com>
2021-12-07 21:09:12 -0600

    ccc

--- aplicacionpruebagithub/src/aplicacionpruebagithub/Aplicacionpruebagithub.java
+++ aplicacionpruebagithub/src/aplicacionpruebagithub/Aplicacionpruebagithub.java
@@ -15,7 +15,8 @@ public class Aplicacionpruebagithub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("fvfv");
+        System.out.println("vvvvv");
         // TODO code application logic here
     }
     
